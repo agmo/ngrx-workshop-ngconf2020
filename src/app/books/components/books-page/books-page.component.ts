9945bfcd99be08b465412932ab3bbd0c81d77afe
agmo <mor.agnieszka@gmail.com>
2021-09-29 22:01:03 +0200

    Challenge 9: effects

--- src/app/books/components/books-page/books-page.component.ts
+++ src/app/books/components/books-page/books-page.component.ts
@@ -25,14 +25,7 @@ export class BooksPageComponent implements OnInit {
 
   ngOnInit() {
     this.store.dispatch(BooksPageActions.enter());
-    this.getBooks();
     this.removeSelectedBook();
-  }
-
-  getBooks() {
-    this.booksService.all().subscribe(books => {
-      this.store.dispatch(BooksApiActions.booksLoaded({ books }));
-    });
   }
 
   onSelect(book: BookModel) {
@@ -59,7 +52,6 @@ export class BooksPageComponent implements OnInit {
     this.store.dispatch(BooksPageActions.createBook({book: bookProps}))
     this.booksService.create(bookProps).subscribe(book => {
       this.store.dispatch(BooksApiActions.bookCreated({ book }));
-      this.getBooks();
       this.removeSelectedBook();
     });
   }
@@ -68,7 +60,6 @@ export class BooksPageComponent implements OnInit {
     this.store.dispatch(BooksPageActions.updateBook({bookId: book.id, changes: book}));
     this.booksService.update(book.id, book).subscribe(book => {
       this.store.dispatch(BooksApiActions.bookUpdated({ book }));
-      this.getBooks();
       this.removeSelectedBook();
     });
   }
@@ -77,7 +68,6 @@ export class BooksPageComponent implements OnInit {
     this.store.dispatch(BooksPageActions.deleteBook({bookId: book.id}));
     this.booksService.delete(book.id).subscribe(() => {
       this.store.dispatch(BooksApiActions.bookDeleted({ bookId: book.id }));
-      this.getBooks();
       this.removeSelectedBook();
     });
   }
