83571ecdae6ce9fc47763c84b334d086f6ba9b3b
agmo <mor.agnieszka@gmail.com>
2021-09-29 13:36:14 +0200

    Challenge 6: selectors

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -1,5 +1,5 @@
-import { Action, createReducer, on } from '@ngrx/store';
-import { BookModel } from 'src/app/shared/models';
+import { Action, createReducer, createSelector, on } from '@ngrx/store';
+import { BookModel, calculateBooksGrossEarnings } from 'src/app/shared/models';
 import { BooksPageActions, BooksApiActions } from 'src/app/books/actions';
 
 // These methods modify the state in an immutable way:
@@ -70,3 +70,30 @@ const booksReducer = createReducer(
 export function reducer(state: State | undefined, action: Action) {
   return booksReducer(state, action);
 }
+
+/**
+ * "Getter" Selectors (simple selectors that just return a property on state)
+ */
+export const selectAll = (state: State) => state.collection;
+export const selectActiveBookId = (state: State) => state.activeBookId;
+
+/**
+ * Complex Selectors (merge multiple - up to 8 - inputs into a result)
+ */
+export const selectActiveBook = createSelector(
+  selectAll,
+  selectActiveBookId,
+  (books, activeBookId) => books.find(book => book.id === activeBookId) || null
+);
+
+const selectEarningsTotals_unoptimized = (state: State) => {
+  const books = selectAll(state);
+
+  return calculateBooksGrossEarnings(books);
+}
+
+// This is the more optimised version because NgRx will only call the projector function if selectAll changes.
+export const selectEarningsTotals = createSelector(
+  selectAll,
+  calculateBooksGrossEarnings // A shorthand for a projector function that takes in "books".
+);
