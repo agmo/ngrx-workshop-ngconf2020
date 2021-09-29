7da8f987911ba22845d764caaadbceb4ba96a39d
agmo <mor.agnieszka@gmail.com>
2021-09-29 14:05:05 +0200

    Challenge 7: export selectors

--- src/app/shared/state/index.ts
+++ src/app/shared/state/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducerMap, createSelector, MetaReducer } from '@ngrx/store';
 import * as fromBooks from './books.reducer';
 
 export interface State {
@@ -10,3 +10,23 @@ export const reducers: ActionReducerMap<State> = {
 };
 
 export const metaReducers: MetaReducer<State>[] = [];
+
+/**
+ * Books Selectors
+ */
+export const selectBooksState = (state: State) => state.books;
+
+export const selectAllBooks = createSelector(
+  selectBooksState,
+  fromBooks.selectAll
+);
+
+export const selectActiveBook = createSelector(
+  selectBooksState,
+  fromBooks.selectActiveBook
+);
+
+export const selectBooksEarningsTotals = createSelector(
+  selectBooksState,
+  fromBooks.selectEarningsTotals
+);
